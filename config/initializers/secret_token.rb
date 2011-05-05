dd728c7c1874635a3a2f34ca0ebb98f4a76ff60c
Dan Bercht <d.bercht@gmail.com>
2011-05-05 14:09:15 -0400

    Initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeatherTunes::Application.config.secret_token = 'c7e6923bc645d77bb98362346e9b091581311b13c08e100b57e9a589daa1af937a0bb24704c1080e9f7bade8ed835362cde46c1c8a7f642f04b31f22f5f333d7'
